bf4433afe0b7e839ff0b237e735ddcac53b12ee1
RudyAttia <rudy.a24@hotmail.fr>
2019-02-17 23:45:32 +0200

    modification of the js

--- project1.js
+++ project1.js
@@ -1,30 +1,30 @@
-document.getElementById('dateform').valueAsDate = new Date();
-document.getElementById('dateform').min = new Date().toISOString().split("T")[0];
+
 var memos = [];
 
 window.onload = function(){
+    document.getElementById('dateform').valueAsDate = new Date();
+    document.getElementById('dateform').min = new Date().toISOString().split("T")[0];
     if(JSON.parse(localStorage.getItem("notes")) === null){return}
     var strg_notes = localStorage.getItem("notes");
     memos = JSON.parse(strg_notes);
-    UpdatePanel();
+    for(var i=memos.length-1;i>=0;i--){
+        UpdatePaper(i);
+    }
 }
 
 function AddMemo() {
-    if(!VerifInfo()){
-        return   
-    }
+    if(!VerifInfo()){return}
     
     var mtitle = document.getElementById("titleform").value;
     var mnote = document.getElementById("noteform").value;
     var mdate = document.getElementById("dateform").value;
     var mtime = document.getElementById("timeform").value;
     mnote = mnote.replace(/\n\r?/g, '<br/>');
-
     var memo = `{"title":"${mtitle}", "note":"${mnote}", "date":"${mdate}", "time":"${mtime}"}`;
-    memos.push(JSON.parse(memo));
+    memos.unshift(JSON.parse(memo));
     EnterLocalStr();
     ClearForm();
-    UpdatePanel();
+    UpdatePaper(0);
 }
 
 function EnterLocalStr(){
@@ -32,19 +32,16 @@ function EnterLocalStr(){
     localStorage.setItem("notes",arr_notes);
 }
 
-function UpdatePanel(){
-    if(memos.length<1){return}
-    var buffer = "";
-    for(var i=(memos.length)-1;i>=0;i--){
-        buffer += `<div class="paperc">
-                        <i class="far fa-times-circle fa-lg" onclick="DeletePaper(${i})"></i>
-                        <h5 class="titlepaper">${memos[i].title}</h5>
-                        <div class="notepaper">${memos[i].note}</div>
-                        <div class="datepaper">${memos[i].date.substring(8,10)}/${memos[i].date.substring(5,7)}/${memos[i].date.substring(0,4)}</div>
-                        <div class="timepaper">${memos[i].time}</div>
-                    </div>`;
-    }
-    document.getElementById("paperboard").innerHTML = buffer;
+function UpdatePaper(indexmemos){
+    var paperboard = document.getElementById("paperboard")
+    var newpaper = document.createElement("div");
+    newpaper.className = "paperc";
+    newpaper.innerHTML = `<i class="far fa-times-circle fa-lg" onclick="DeletePaper(event)"></i>
+                          <h5 class="titlepaper">${memos[indexmemos].title}</h5>
+                          <div class="notepaper">${memos[indexmemos].note}</div>
+                          <div class="datepaper">${memos[indexmemos].date.substring(8,10)}/${memos[indexmemos].date.substring(5,7)}/${memos[indexmemos].date.substring(0,4)}</div>
+                          <div class="timepaper">${memos[indexmemos].time}</div>`
+    paperboard.insertBefore(newpaper,paperboard.firstChild);
 }
 
 function ClearForm() {
@@ -57,10 +54,11 @@ function ClearForm() {
     document.getElementById("timeform").value = "";
 }
 
-function DeletePaper(index){
+function DeletePaper(e){
+    var index = Array.from(e.target.parentElement.parentElement.children).indexOf(e.target.parentElement);
+    e.target.parentElement.parentElement.removeChild(e.target.parentElement);
     memos.splice(index,1);
     EnterLocalStr();
-    UpdatePanel();
 }
 
 function VerifInfo() {
@@ -78,7 +76,7 @@ function VerifInfo() {
     }
     var datetime = "";
     if (mtime !== "") {
-        datetime = "T" + mtime + ":00";
+        datetime = "T" + mtime + ":59";
     }
     else {
         datetime = "T23:59:59";
